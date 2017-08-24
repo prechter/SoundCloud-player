3629fdadc9899ddc164387895333463e3257d742
Michael Prechter <m.r.prechter@gmail.com>
2017-08-24 14:01:05 -0400

    changes made to music player

--- main.js
+++ main.js
@@ -1,75 +1,68 @@
-let musicPlayer = document.getElementById('musicplayer');
-// let trackSearch = document.getElementById('search-field');
-let searchButton = document.getElementById('searchbutton');
-let searchField = document.getElementById('searchfield');
+const musicPlayer = document.querySelector('#music-player');
+const nowPlaying = document.querySelector('#now-playing')
+// const searchField = document.querySelector('#search-field');
+const results = document.querySelector('#track-results');
+let dataFetch = {};
+let string = ''
 
-searchButton.addEventListener('click', searchUser);
+// searchButton.addEventListener('click', searchTracks);
 
-function searchUser() {
+function searchTracks() {
   event.preventDefault();
   // preventDefault stops the page from reloading which was causing a break somewhere in the function(note: this can also be fixed by changing the "form" to a "div")
-  let string = searchField.value;
-  console.log(string);
-  let userAPI = 'http://api.soundcloud.com/users/?client_id=8538a1744a7fdaa59981232897501e04&q=' + string;
-
-fetch(userAPI)
-  .then(
-    function(response) {
-      if (response.status !== 200) {
-        console.log('Uh-oh music fan! Looks like a ' + response.status);
-        return;
-      }
-      response.json().then(function(data) {
-        console.log(data[0]);
-        document.getElementById('userresults').innerHTML = '<h1>USERS</h1>';
-        for (var i = 0; i < data.length; i++) {
-          let markup = `
-                <div class="user-wrapper">
-                  <img class="user-avatar" src="${data[i].avatar_url}" width=150vw height=150vw>
-                  <br><br>
-                  <a href="${data[i].permalink_url}" class="username">${data[i].username}</a>
-                </div><br>`
-        document.getElementById('userresults').innerHTML +=  markup;
-        }
-        return;
-        })
-      }
-    )
-
+  string = document.querySelector('#search-field').value;
   let trackAPI = 'http://api.soundcloud.com/tracks/?client_id=8538a1744a7fdaa59981232897501e04&q=' + string;
 
-fetch(trackAPI)
-  .then(
-    function(response) {
-      if (response.status !== 200) {
-        console.log('Uh-oh music fan! Looks like a ' + response.status);
-        return;
-      }
-      response.json().then(function(data) {
-        document.getElementById('trackresults').innerHTML = '<h1>TRACKS</h1>';
-        for (var i = 0; i < data.length; i++) {
-          let markup = `
-                <div class="user-wrapper">
-                  <img class="user-avatar" src="${data[i].user.avatar_url}" width=150vw height=150vw>
-                  <br><br>
-                  <a href="${data[i].permalink_url}" class="username">${data[i].user.username}</a><br>
-                  <a href="${data[i].stream_url}" class="song-title">${data[i].title}</a>
-                </div><br>`
-        document.getElementById('trackresults').innerHTML += markup;
+  fetch(trackAPI)
+    .then(
+      function(response) {
+        if (response.status !== 200) {
+          console.log('Uh-oh music fan! Looks like there was a problem. Status: ' + response.status);
+          return;
         }
-        return;
+        response.json().then(function(data) {
+          console.log(trackAPI);
+          dataFetch = data
+          console.log(dataFetch);
+          function showResults() {
+            // let avatar = data.user.avatar_url;
+            // let title = data.title;
+            // let permaLink = data.permalink_url;
+            // let likes = data.likes_count;
+            // let username = data.user.username;
+            // let streamLink = data.stream_url;
+
+            return `${dataFetch.map(track =>
+              `<div class="song-wrapper" id="${track.id}" >
+                <img class="user-avatar" src="${track.artwork_url}" width=100vw height=100vw data-src="${track.stream_url}" data-title="${track.title}">
+                <br>
+                <div class="artist-track">
+                    <p class="username" data-src="${track.stream_url}" data-title="${track.title}">${track.user.username}</p>
+                    <span class="song-title" data-src="${track.stream_url}" data-title="${track.title}">${track.title}</span>
+                    <br>
+                    <span class="release-year">${track.likes_count} likes</span>
+                </div>
+              </div>
+              <br>
+              <hr>`
+            ).join('')}`
+          }
+
+          let markup = `${showResults()}`;
+          results.innerHTML = markup;
         })
       }
     )
-};
+  }
 
-let songSelect = getElementsByClassName('user-wrapper');
 
-songSelect.addEventListener('click', playMusic);
+results.addEventListener('click', playTrack);
 
-function playMusic() {
-  let stream = document.getElementsByClassName('song-title');
-  let song = stream.value;
-  let songLink = song + '?client_id=8538a1744a7fdaa59981232897501e04';
-  musicPlayer.setAttribute("src", songLink).play();
+function playTrack(e) {
+  var trackSource = e.target.getAttribute("data-src");
+  console.log(trackSource);
+  var trackPlaying = e.target.getAttribute("data-title");
+  console.log(trackPlaying);
+  nowPlaying.innerHTML = "Now playing: " + trackPlaying;
+  musicPlayer.setAttribute("src", trackSource + "?client_id=8538a1744a7fdaa59981232897501e04");
 }
